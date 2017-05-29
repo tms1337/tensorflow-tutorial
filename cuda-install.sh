fd46219baaa33595276c7fee42f37d02e959d017
tms1337 <faruk1337@gmail.com>
2017-05-29 06:50:15 +0200

    prepare cuda install script

---
+++ cuda-install.sh
@@ -0,0 +1,46 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev libcurl3-dev
+sudo pip install -U pip
+
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+rm cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+sudo apt-get update
+sudo apt-get install -y cuda
+
+sudo dpkg -i libcudnn5_5.1.5-1+cuda8.0_amd64.deb
+sudo dpkg -i libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb
+
+export CUDA_HOME=/usr/local/cuda
+export CUDA_ROOT=/usr/local/cuda
+export PATH=$PATH:$CUDA_ROOT/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
+
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+# Hack to silently agree license agreement
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install bazel
+sudo apt-get upgrade bazel
+
+bazel version
+
+git clone --recurse-submodules https://github.com/tensorflow/tensorflow
+
+cd tensorflow
+TF_UNOFFICIAL_SETTING=1 ./configure
+
+bazel build -c opt --config=cuda //tensorflow/cc:tutorials_example_trainer
+bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+
+
+sudo pip install --upgrade /tmp/tensorflow_pkg/tensorflow-0.11.0rc0-py2-none-any.whl
+
+python ~/tensorflow/tensorflow/models/image/mnist/convolutional.py
