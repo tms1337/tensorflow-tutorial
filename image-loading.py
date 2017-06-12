c4813f8836fb3d97967d2a28d5a139690be0f457
tms1337 <faruk1337@gmail.com>
2017-06-13 01:19:20 +0200

    remove mnist data. createwang-cnn.py

--- image-loading.py
+++ image-loading.py
@@ -1,21 +1,23 @@
-# import numpy
-# import pylab
-# from PIL import Image
-#
-# # open random image of dimensions 639x516
-# img = Image.open(open('doc/images/3wolfmoon.jpg'))
-# # dimensions are (height, width, channel)
-# img = numpy.asarray(img, dtype='float64') / 256.
-#
-# # put image in 4D tensor of shape (1, 3, height, width)
-# img_ = img.transpose(2, 0, 1).reshape(1, 3, 639, 516)
-# filtered_img = f(img_)
-#
-# # plot original image and first and second components of output
-# pylab.subplot(1, 3, 1); pylab.axis('off'); pylab.imshow(img)
-# pylab.gray();
-# # recall that the convOp output (filtered image) is actually a "minibatch",
-# # of size 1 here, so we take index 0 in the first dimension:
-# pylab.subplot(1, 3, 2); pylab.axis('off'); pylab.imshow(filtered_img[0, 0, :, :])
-# pylab.subplot(1, 3, 3); pylab.axis('off'); pylab.imshow(filtered_img[0, 1, :, :])
-# pylab.show()
+from __future__ import division, print_function
+
+import numpy
+import pylab
+from PIL import Image
+
+img = Image.open(open("/home/faruk/Desktop/wang1000/0/0.jpg"))
+img = numpy.asarray(img, dtype='float64') / 256.
+
+print(img)
+
+# put image in 4D tensor of shape (1, 3, height, width)
+img_tensor = img.transpose(2, 0, 1).reshape(1, 3, 384, 256)
+filtered_img = img_tensor
+
+# plot original image and first and second components of output
+pylab.subplot(1, 3, 1); pylab.axis('off'); pylab.imshow(img)
+pylab.gray()
+# recall that the convOp output (filtered image) is actually a "minibatch",
+# of size 1 here, so we take index 0 in the first dimension:
+pylab.subplot(1, 3, 2); pylab.axis('off'); pylab.imshow(filtered_img[0, 0, :, :])
+pylab.subplot(1, 3, 3); pylab.axis('off'); pylab.imshow(filtered_img[0, 1, :, :])
+pylab.show()
