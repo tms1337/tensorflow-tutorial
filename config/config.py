f6aebba201247e27e9796a1c0d00df0f5d1b2993
tms1337 <faruk1337@gmail.com>
2017-06-16 06:29:35 +0200

    add pairwise training data generation

--- config/config.py
+++ config/config.py
@@ -1,12 +1,24 @@
-is_local = False
+import os
+
+is_local = "LOCAL" in os.environ
 
 if is_local:
     config = {
         "output_dir": ".",
-        "data_root_dir": "/home/faruk/Desktop/wang1000"
+        "data_root_dir": "/home/faruk/Desktop/experiment_root",
+        "inception-top": {
+            "batch_size": 2,
+            "epoch_n": 2,
+            "data_n": 5
+        }
     }
 else:
     config = {
         "output_dir": "/output",
-        "data_root_dir": "/input"
+        "data_root_dir": "/input",
+        "inception-top": {
+            "batch_size": 1000,
+            "epoch_n": 500,
+            "data_n": 10000
+        }
     }
--- retrain-inception.py
+++ retrain-inception.py
@@ -4,70 +4,63 @@ from dlm.inception_v3 import InceptionV3
 from config.config import config
 from keras.preprocessing.image import load_img, ImageDataGenerator, img_to_array
 from keras.layers import Flatten, Dense, Dropout
-from keras.models import Model, Sequential
-from keras.losses import binary_crossentropy, categorical_crossentropy
+from keras.models import Model, Sequential, model_from_json
+from keras.losses import binary_crossentropy, categorical_crossentropy, mean_squared_error
 from keras.optimizers import Adam, Adadelta
+import keras.backend as K
 
 print("Creating data")
 batch_size = 1
 train_datagen = ImageDataGenerator(rescale=1.0 / 255,
-                                   # rotation_range=45,
-                                   # width_shift_range=0.2,
-                                   # height_shift_range=0.2,
-                                   # zoom_range=0.5,
-                                   # fill_mode="nearest",
-                                   # horizontal_flip=True,
-                                   # shear_range=0.2)
-                                   )
-data_generator = train_datagen.flow_from_directory(config["data_root_dir"],
+                                   rotation_range=45,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   zoom_range=0.5,
+                                   fill_mode="nearest",
+                                   horizontal_flip=True,
+                                   shear_range=0.2)
+data_path = "%s/%s" % (config["data_root_dir"], "wang999")
+data_generator = train_datagen.flow_from_directory(data_path,
                                                    target_size=(384, 256),
                                                    batch_size=batch_size,
                                                    shuffle=True)
 
-total = 1000
-train_perc = 0.9
-
 print("Loading inception net")
-inception = InceptionV3(include_top=False, input_shape=(384, 256, 3))
+inception_model_path = "%s/%s/%s" % (config["data_root_dir"], "inception_model", "inception.json")
+inception_file = open(inception_model_path, "r")
+inception_json = inception_file.read()
+inception_file.close()
+inception = model_from_json(inception_json)
+inception_weights_path = "%s/%s/%s" % (config["data_root_dir"], "inception_model", "inception.h5")
+inception.load_weights(inception_weights_path)
 
-train_n = int(train_perc * total)
-train_data_x = inception.predict_generator(data_generator, train_n)
-
-class_n = 9
-
-train_data_y = np.ndarray((train_n, class_n))
-data_generator.reset()
-for i in range(train_n):
-    item = data_generator.next()
-    train_data_y[i] = item[1]
-
-test_n = int( (1 - train_perc) * total )
-test_data_x = inception.predict_generator(data_generator, test_n)
-test_data_y = np.ndarray((test_n, class_n))
-
-for i in range(test_n):
-    item = data_generator.next()
-    test_data_y[i] = item[1]
+code_length = 1024
 
 model = Sequential()
-model.add(Flatten(input_shape=train_data_x.shape[1:]))
-model.add(Dense(8, activation="relu"))
+model.add(Flatten(input_shape=(10, 6, 2048)))
 model.add(Dropout(0.5))
-model.add(Dense(4, activation="relu"))
-model.add(Dropout(0.3))
-model.add(Dense(class_n, activation="softmax"))
+model.add(Dense(code_length, activation="softmax"))
 
-model.compile(optimizer=Adam(lr=0.00006),
-              loss=binary_crossentropy,
-              metrics=["accuracy"])
+model.compile(optimizer=Adam(lr=0.0003),
+              loss=mean_squared_error,
+              metrics=[mean_squared_error])
 
-print("Full model compiled")
+data_n = config["inception-top"]["data_n"]
+train_x = np.ndarray((data_n, 10, 6, 2048))
+train_y = np.ndarray((data_n, code_length))
+for i in range(data_n):
+    a = data_generator.next()
+    b = data_generator.next()
 
-model.fit(train_data_x,
-          train_data_y,
-          batch_size=train_n,
-          epochs=300,
-          verbose=1,
-          validation_data=(test_data_x, test_data_y))
+    train_x[i] = inception.predict(a[0])
 
-print("Model fit")
+    y_intermediate = inception.predict(b[0])
+    y = np.round(model.predict(y_intermediate), 0)
+    if not np.array_equal(a[1], b[1]):
+        y = 1 - y
+
+    train_y[i] = y
+
+batch_size = config["inception-top"]["batch_size"]
+epoch_n = config["inception-top"]["epoch_n"]
+model.fit(train_x, train_y, batch_size=batch_size, epochs=epoch_n)
