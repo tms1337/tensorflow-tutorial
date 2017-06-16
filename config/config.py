5d90458d64b6612393464729d0dc06e6ae270b57
tms1337 <faruk1337@gmail.com>
2017-06-16 17:36:39 +0200

    add deep convolutional autoencoder

--- config/config.py
+++ config/config.py
@@ -8,8 +8,9 @@ if is_local:
         "data_root_dir": "/home/faruk/Desktop/experiment_root",
         "inception-top": {
             "batch_size": 2,
-            "epoch_n": 2,
-            "data_n": 5
+            "epoch_n": 5,
+            "data_n": 10,
+            "repeat-n": 1
         }
     }
 else:
@@ -17,8 +18,9 @@ else:
         "output_dir": "/output",
         "data_root_dir": "/input",
         "inception-top": {
-            "batch_size": 1000,
-            "epoch_n": 500,
-            "data_n": 10000
+            "batch_size": 500,
+            "epoch_n": 10,
+            "data_n": 5000,
+            "repeat-n": 1
         }
     }
--- retrain-inception.py
+++ retrain-inception.py
@@ -3,13 +3,17 @@ import numpy as np
 from dlm.inception_v3 import InceptionV3
 from config.config import config
 from keras.preprocessing.image import load_img, ImageDataGenerator, img_to_array
-from keras.layers import Flatten, Dense, Dropout
+from keras.layers import Flatten, Dense, Dropout, MaxPooling2D, Conv2D, UpSampling2D, Lambda
 from keras.models import Model, Sequential, model_from_json
 from keras.losses import binary_crossentropy, categorical_crossentropy, mean_squared_error
 from keras.optimizers import Adam, Adadelta
 import keras.backend as K
+import sys
 
 print("Creating data")
+width = 256
+height = 256
+
 batch_size = 1
 train_datagen = ImageDataGenerator(rescale=1.0 / 255,
                                    rotation_range=45,
@@ -21,46 +25,54 @@ train_datagen = ImageDataGenerator(rescale=1.0 / 255,
                                    shear_range=0.2)
 data_path = "%s/%s" % (config["data_root_dir"], "wang999")
 data_generator = train_datagen.flow_from_directory(data_path,
-                                                   target_size=(384, 256),
+                                                   target_size=(width, height),
                                                    batch_size=batch_size,
                                                    shuffle=True)
 
-print("Loading inception net")
-inception_model_path = "%s/%s/%s" % (config["data_root_dir"], "inception_model", "inception.json")
-inception_file = open(inception_model_path, "r")
-inception_json = inception_file.read()
-inception_file.close()
-inception = model_from_json(inception_json)
-inception_weights_path = "%s/%s/%s" % (config["data_root_dir"], "inception_model", "inception.h5")
-inception.load_weights(inception_weights_path)
+autoencoder = Sequential()
 
-code_length = 1024
+autoencoder.add(Conv2D(32, (2, 2), activation="relu", padding="same", input_shape=(width, height, 3)))
+autoencoder.add(MaxPooling2D((4, 4)))
 
-model = Sequential()
-model.add(Flatten(input_shape=(10, 6, 2048)))
-model.add(Dropout(0.5))
-model.add(Dense(code_length, activation="softmax"))
+autoencoder.add(Conv2D(32, (2, 2), activation="relu", padding="same"))
+autoencoder.add(MaxPooling2D((4, 4)))
 
-model.compile(optimizer=Adam(lr=0.0003),
-              loss=mean_squared_error,
-              metrics=[mean_squared_error])
+autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
+autoencoder.add(MaxPooling2D((4, 4)))
 
-data_n = config["inception-top"]["data_n"]
-train_x = np.ndarray((data_n, 10, 6, 2048))
-train_y = np.ndarray((data_n, code_length))
-for i in range(data_n):
-    a = data_generator.next()
-    b = data_generator.next()
+autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
+autoencoder.add(MaxPooling2D((4, 4)))
 
-    train_x[i] = inception.predict(a[0])
+autoencoder.add(Dropout(0.5))
 
-    y_intermediate = inception.predict(b[0])
-    y = np.round(model.predict(y_intermediate), 0)
-    if not np.array_equal(a[1], b[1]):
-        y = 1 - y
+autoencoder.add(Conv2D(32, (1, 1), activation="softmax", padding="same"))
+autoencoder.add(UpSampling2D((4, 4)))
 
-    train_y[i] = y
+autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
+autoencoder.add(UpSampling2D((4, 4)))
 
-batch_size = config["inception-top"]["batch_size"]
+autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
+autoencoder.add(UpSampling2D((4, 4)))
+
+autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
+autoencoder.add(UpSampling2D((4, 4)))
+
+autoencoder.add(Dropout(0.5))
+
+autoencoder.add(Conv2D(3, (4, 4), activation="softmax", padding="same"))
+
+autoencoder.compile(optimizer=Adam(lr=0.001),
+                    loss=binary_crossentropy,
+                    metrics=[binary_crossentropy])
+
+total = config["inception-top"]["data_n"]
+data = np.ndarray((total, width, height, 3))
+for i in range(total):
+    data[i] = data_generator.next()[0]
+
+print(data)
+print(data.shape)
+
 epoch_n = config["inception-top"]["epoch_n"]
-model.fit(train_x, train_y, batch_size=batch_size, epochs=epoch_n)
+batch_size = config["inception-top"]["batch_size"]
+autoencoder.fit(data, data, batch_size=batch_size, epochs=epoch_n)
