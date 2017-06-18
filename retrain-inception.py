fix scaling bug

retrain-inception.py
@@ -8,13 +8,14 @@ from keras.models import Model, Sequential, model_from_json
 from keras.losses import binary_crossentropy, categorical_crossentropy, mean_squared_error
 from keras.optimizers import Adam, Adadelta
 import keras.backend as K
+from keras.datasets import cifar100
 import sys
 
-print("Creating data")
-width = 256
-height = 256
+# print("Creating data")
+# width = 256
+# height = 256
 
-batch_size = 1
+# batch_size = 1
 train_datagen = ImageDataGenerator(rescale=1.0 / 255,
                                    rotation_range=45,
                                    width_shift_range=0.2,
@@ -23,56 +24,72 @@ train_datagen = ImageDataGenerator(rescale=1.0 / 255,
                                    fill_mode="nearest",
                                    horizontal_flip=True,
                                    shear_range=0.2)
-data_path = "%s/%s" % (config["data_root_dir"], "wang999")
-data_generator = train_datagen.flow_from_directory(data_path,
-                                                   target_size=(width, height),
-                                                   batch_size=batch_size,
-                                                   shuffle=True)
+#                                    )
+# data_path = "%s/%s" % (config["data_root_dir"], "wang999")
+# data_generator = train_datagen.flow_from_directory(data_path,
+#                                                    target_size=(width, height),
+#                                                    batch_size=batch_size,
+#                                                    shuffle=True)
+
+
+
+(x_train, _), (x_test, _) = cifar100.load_data(label_mode='fine')
+x_train = x_train.astype("float32") * (1.0/255)
+x_test = x_test.astype("float32") * (1.0/255)
+
+
+code_length = 128
 
 autoencoder = Sequential()
 
-autoencoder.add(Conv2D(32, (2, 2), activation="relu", padding="same", input_shape=(width, height, 3)))
-autoencoder.add(MaxPooling2D((4, 4)))
+autoencoder.add(Conv2D(128, (8, 8), activation="relu", padding="same", input_shape=x_train.shape[1:]))
+autoencoder.add(MaxPooling2D((2, 2)))
 
+autoencoder.add(Conv2D(64, (8, 8), activation="relu", padding="same"))
+autoencoder.add(MaxPooling2D((2, 2)))
+
+autoencoder.add(Conv2D(32, (4, 4), activation="relu", padding="same"))
 autoencoder.add(Conv2D(32, (2, 2), activation="relu", padding="same"))
-autoencoder.add(MaxPooling2D((4, 4)))
+autoencoder.add(MaxPooling2D((2, 2)))
 
 autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
-autoencoder.add(MaxPooling2D((4, 4)))
-
-autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
-autoencoder.add(MaxPooling2D((4, 4)))
+autoencoder.add(MaxPooling2D((2, 2)))
 
 autoencoder.add(Dropout(0.5))
 
-autoencoder.add(Conv2D(32, (1, 1), activation="softmax", padding="same"))
-autoencoder.add(UpSampling2D((4, 4)))
-
+autoencoder.add(UpSampling2D((2, 2)))
 autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
-autoencoder.add(UpSampling2D((4, 4)))
 
-autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
-autoencoder.add(UpSampling2D((4, 4)))
+autoencoder.add(UpSampling2D((2, 2)))
+autoencoder.add(Conv2D(32, (4, 4), activation="relu", padding="same"))
+autoencoder.add(Conv2D(32, (4, 4), activation="relu", padding="same"))
 
-autoencoder.add(Conv2D(16, (2, 2), activation="relu", padding="same"))
-autoencoder.add(UpSampling2D((4, 4)))
+autoencoder.add(UpSampling2D((2, 2)))
+autoencoder.add(Conv2D(64, (4, 4), activation="relu", padding="same"))
 
-autoencoder.add(Dropout(0.5))
-
+autoencoder.add(UpSampling2D((2, 2)))
+autoencoder.add(Conv2D(128, (8, 8), activation="relu", padding="same"))
 autoencoder.add(Conv2D(3, (4, 4), activation="softmax", padding="same"))
 
-autoencoder.compile(optimizer=Adam(lr=0.001),
+autoencoder.compile(optimizer=Adam(lr=0.000006),
                     loss=binary_crossentropy,
-                    metrics=[binary_crossentropy])
-
-total = config["inception-top"]["data_n"]
-data = np.ndarray((total, width, height, 3))
-for i in range(total):
-    data[i] = data_generator.next()[0]
-
-print(data)
-print(data.shape)
+                    metrics=[binary_crossentropy, mean_squared_error])
 
 epoch_n = config["inception-top"]["epoch_n"]
 batch_size = config["inception-top"]["batch_size"]
-autoencoder.fit(data, data, batch_size=batch_size, epochs=epoch_n)
+steps_per_epoch = config["inception-top"]["steps_per_epoch"]
+
+data_generator = train_datagen.flow(x_train, x_train, shuffle=True, batch_size=batch_size, steps_per_epoch=100)
+
+autoencoder.fit_generator(data_generator, validation_data=(x_test, x_test))
+
+# autoencoder.fit(x_train, x_train,
+#                 batch_size=batch_size,
+#                 epochs=epoch_n,
+#                 shuffle=True,
+#                 validation_data=(x_test, x_test))
+
+model_json = autoencoder.to_json()
+with open("%s/%s" % (config["output_dir"], "autoencoder.json"), "w+") as model_file:
+    model_file.write(model_json)
+autoencoder.save_weights("%s/%s" % (config["output_dir"], "autoencoder.h5"))

test_model.py
@@ -11,23 +11,29 @@ from PIL import Image
 import os
 import random
 import matplotlib.pyplot as plt
+from keras.datasets import cifar100
 
-model_url = "/home/faruk/Desktop/output/full.json"
+file_name = "autoencoder"
+
+model_url = "/home/faruk/Desktop/output/%s.json" % file_name
 model_file = open(model_url, "r")
 model_json = model_file.read()
 model_file.close()
 model = model_from_json(model_json)
-model.load_weights("/home/faruk/Desktop/output/full.h5")
+model.load_weights("/home/faruk/Desktop/output/%s.h5" % file_name)
 
-img = Image.open(open("/home/faruk/Desktop/wang1000/1/132.jpg", "rb"))
-img = np.asarray(img, dtype='float64') / 256.
+(x_train, _), (x_test, _) = cifar100.load_data(label_mode='fine')
 
-plt.imshow(img)
-plt.show()
+for i in range(10, 20):
+    img = x_train[i]
 
-img_input = img.reshape((1, 384*256*3))
-decoded_img_tensor = 256.0 * model.predict(img_input)
+    plt.imshow(img)
+    plt.show()
 
-decoded_img = decoded_img_tensor.reshape( (384, 256, 3) )
-plt.imshow(decoded_img)
-plt.show()
+    prediction = model.predict(x_train[i:i + 1])
+    print(prediction.shape)
+    print(prediction)
+    img = prediction[0]
+
+    plt.imshow(img)
+    plt.show()